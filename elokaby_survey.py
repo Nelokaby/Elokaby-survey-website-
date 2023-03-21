3b9be7c150001cd402f51c90e3c62487cfadad1d
Nada Mahmoud Elokby <nadaelokby@gmail.com>
2023-03-21 20:00:44 +0200

    logo is edited

--- elokaby_survey.py
+++ elokaby_survey.py
@@ -81,6 +81,6 @@ def result():
     return render_template("result.html", questions=data,rate=rate)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=9000)
+    app.run(debug=True, port=8000)
 
 
